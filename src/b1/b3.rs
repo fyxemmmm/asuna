c5f112452ca5b7f811e03f6edc9742d3bf510470
fyxemmmm <979745517@qq.com>
2021-10-01 21:40:39 +0800

    rs

---
+++ src/b1/b3.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("state quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn main() {
+    let c = Coin::Quarter(UsState::Alaska);
+    println!("{}", value_in_cents(c));
+}
+
